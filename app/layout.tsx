5465a0fb3bf73bbdb9888fd53096d30b32731126
b-at-neu <winkler.b@northeastern.edu>
2024-07-23 23:22:51 +0200

    #11: add body styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function Layout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body>
+      <body className='w-full min-h-screen bg-slate-900'>
         <main>{children}</main>
       </body>
     </html>
