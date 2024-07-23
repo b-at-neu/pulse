ecd116808b8f922a629a6d78989a19823b5610cd
b-at-neu <winkler.b@northeastern.edu>
2024-07-23 23:25:35 +0200

    #11: change bg color

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function Layout({
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
-      <body className='w-full min-h-screen bg-slate-900'>
+      <body className='w-full min-h-screen bg-slate-950'>
         <Navbar />
         <main>{children}</main>
       </body>
