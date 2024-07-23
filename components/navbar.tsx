fa8ce7b11d3224c1c6c1dca974287bbce8c73b4d
b-at-neu <winkler.b@northeastern.edu>
2024-07-23 23:41:52 +0200

    #11: fix home link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ export function Navbar() {
   return (
     <nav className="h-12 bg-slate-900 flex justify-center">
       <div className="w-3/5 h-full flex flex-row justify-around items-center">
-        <NavbarLink text={'home'} href={''} />
+        <NavbarLink text={'home'} href={'/'} />
         <NavbarLink text={'students'} href={'/students'} />
         <NavbarLink text={'lockers'} href={'/lockers'} />
         <NavbarLink text={'locks'} href={'/locks'} />
