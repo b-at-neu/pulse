4b0075cb4da5772d31b4839bdd789182ee446d51
b-at-neu <winkler.b@northeastern.edu>
2024-07-23 23:17:31 +0200

    #11: add html head

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,10 @@ export default function Layout({
 }>) {
   return (
     <html>
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body>
         <main className="">{children}</main>
       </body>
