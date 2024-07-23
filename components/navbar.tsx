1f373d8898eb2675f8440cd7d783765cd9951c4c
b-at-neu <winkler.b@northeastern.edu>
2024-07-23 23:34:11 +0200

    #11: create navbar layout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,3 +1,7 @@
+import Link from "next/link";
+
 export function Navbar() {
-  return <nav className="w-full h-12 bg-slate-900"></nav>;
+  return <nav className="w-full h-12 bg-slate-900 flex flex-row justify-center items-center">
+    <Link className="h-fit hover:text-slate-400" href={""}>Home</Link>
+  </nav>;
 }
