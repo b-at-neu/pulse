e16f78d71ca904b3a7ea71a19d0c5a15c6b78b96
b-at-neu <winkler.b@northeastern.edu>
2024-07-23 23:41:22 +0200

    #11: move navbar links around

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,14 +1,16 @@
-import Link from "next/link";
-import { NavbarLink } from "./navbar-link";
+import Link from 'next/link';
+import { NavbarLink } from './navbar-link';
 
 export function Navbar() {
   return (
-    <nav className="w-full h-12 bg-slate-900 flex flex-row justify-center items-center">
-      <NavbarLink text={'home'} href={''} />
-      <NavbarLink text={'students'} href={'/students'} />
-      <NavbarLink text={'lockers'} href={'/lockers'} />
-      <NavbarLink text={'locks'} href={'/locks'} />
-      <NavbarLink text={'membership'} href={'/membership'} />
+    <nav className="h-12 bg-slate-900 flex justify-center">
+      <div className="w-3/5 h-full flex flex-row justify-around items-center">
+        <NavbarLink text={'home'} href={''} />
+        <NavbarLink text={'students'} href={'/students'} />
+        <NavbarLink text={'lockers'} href={'/lockers'} />
+        <NavbarLink text={'locks'} href={'/locks'} />
+        <NavbarLink text={'membership'} href={'/membership'} />
+      </div>
     </nav>
   );
 }
