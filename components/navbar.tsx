8cb52406e82f2449b4dee1d02560455bbdab89be
b-at-neu <winkler.b@northeastern.edu>
2024-07-23 23:37:17 +0200

    #11: add all links

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -2,7 +2,13 @@ import Link from "next/link";
 import { NavbarLink } from "./navbar-link";
 
 export function Navbar() {
-  return <nav className="w-full h-12 bg-slate-900 flex flex-row justify-center items-center">
-    <NavbarLink text={"Home"} href={""} />
-  </nav>;
+  return (
+    <nav className="w-full h-12 bg-slate-900 flex flex-row justify-center items-center">
+      <NavbarLink text={'home'} href={''} />
+      <NavbarLink text={'students'} href={'/students'} />
+      <NavbarLink text={'lockers'} href={'/lockers'} />
+      <NavbarLink text={'locks'} href={'/locks'} />
+      <NavbarLink text={'membership'} href={'/membership'} />
+    </nav>
+  );
 }
