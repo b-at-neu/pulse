acd886e13cffb68c87e6420623b7e52f2ea51633
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 21:54:13 +0200

    #13 add title

--- app/students/[id]/page.tsx
+++ app/students/[id]/page.tsx
@@ -1,3 +1,21 @@
+import { Title } from "@/components/ui/title";
+
 export default function Page() {
-  return <></>
+  const data = {
+    id: 'e80a33c3-1395-4b5f-8522-3f4c75b8c5a0',
+    firstName: 'Alice',
+    lastName: 'Smith',
+    applicationReceived: true,
+    depositPaid: false,
+    topLocker: true,
+    class: '7b',
+    locker: '#131',
+    lock: 987654321
+  };
+
+  return (
+    <>
+      <Title>{data.lastName}, {data.firstName} ({data.class})</Title>
+    </>
+  );
 }
