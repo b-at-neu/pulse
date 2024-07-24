059196fcd52d8dddd6064ce7efcfeef5b4c102c9
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 22:04:29 +0200

    #13 remove mb from title

--- components/ui/title.tsx
+++ components/ui/title.tsx
@@ -3,5 +3,5 @@ import { cn } from '@/lib/utils';
 interface TitleProps extends React.HTMLAttributes<HTMLElement> {}
 
 export function Title({ children, className }: TitleProps) {
-  return <h1 className={cn('mb-5 text-3xl md:text-4xl font-bold', className)}>{children}</h1>;
+  return <h1 className={cn('text-3xl md:text-4xl font-bold', className)}>{children}</h1>;
 }
