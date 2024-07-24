cbcc27c703e4c8425244cdfe9bc5c41a244ff464
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 21:41:37 +0200

    #13 add link to bar

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -1,11 +1,10 @@
 import { ReactElement } from 'react';
-import { Url } from 'next/dist/shared/lib/router/router';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
-export function Bar({ data, link }: { data: [data: string | ReactElement, width: string][]; link: Url }) {
+export function Bar({ data, link }: { data: [data: string | ReactElement, width: string][]; link: string }) {
   return (
-    <Link className="flex flex-row items-center gap-1 bg-muted hover:bg-muted/70 w-full h-8 border rounded-lg px-3" href={''}>
+    <Link className="flex flex-row items-center gap-1 bg-muted hover:bg-muted/70 w-full h-8 border rounded-lg px-3" href={link}>
       {data.map(([data, width], i) => (
         <div key={i} className={cn(width, 'truncate')}>
           {data}
