f8b218bf5d225a8724359befa635e746c90d5a2e
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 18:40:10 +0200

    #12 switch to array destructuring

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -1,17 +1,10 @@
 import { ReactElement } from 'react';
 import { cn } from '@/lib/utils';
 
-export function Bar({
-  data
-}: {
-  data: {
-    data: string | ReactElement;
-    width: string;
-  }[];
-}) {
+export function Bar({ data }: { data: [data: string | ReactElement, width: string][] }) {
   return (
     <div className="w-full h-8 border rounded-lg bg-muted">
-      {data.map(({ data, width }, i) => (
+      {data.map(([data, width], i) => (
         <div key={i} className={cn(width, 'h-full align-middle')}>
           {data}
         </div>
