1c0042a1651aa452c5603c2ceb981601b1f282aa
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 14:02:04 +0200

    #11: add transition

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import { ChevronDown, ChevronUp } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import { NavbarLinks } from '@/components/navbar-links';
+import { cn } from '@/lib/utils';
 
 export function Navbar() {
   const [open, setOpen] = useState(false);
@@ -13,9 +14,9 @@ export function Navbar() {
       <div className="hidden md:flex flex-row w-3/5 justify-around items-center">
         <NavbarLinks />
       </div>
-      <div className="md:hidden flex flex-col w-full items-center">
+      <div className={cn("md:hidden flex flex-col w-full items-center transition-all", open ? 'h-72' : 'h-12')}>
         {open && <NavbarLinks />}
-        <Button className='w-full' variant="ghost" onClick={() => setOpen(!open)}>
+        <Button className="w-full" variant="ghost" onClick={() => setOpen(!open)}>
           {!open && <ChevronDown />}
           {open && <ChevronUp />}
           <span className="sr-only">Show Pages</span>
