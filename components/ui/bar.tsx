f873e03474c9a8ad3ea22b02908e761c05d1b6b9
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 18:26:14 +0200

    #12 add bar subcomponents

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -1,3 +1,18 @@
-export function Bar() {
-  return <div className="w-full h-8 border rounded-lg bg-muted"></div>;
+export function Bar({
+  data
+}: {
+  data: [
+    {
+      data: string;
+      width: number;
+    }
+  ];
+}) {
+  return (
+    <div className="w-full h-8 border rounded-lg bg-muted">
+      {data.map((v, i) => (
+        <div key={i}></div>
+      ))}
+    </div>
+  );
 }
