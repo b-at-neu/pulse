ac147f6460c337d5383dfe44a88b36b537abf7a2
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 18:36:54 +0200

    #12 fix data array

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -1,17 +1,18 @@
+import { ReactElement } from 'react';
+import { cn } from '@/lib/utils';
+
 export function Bar({
   data
 }: {
-  data: [
-    {
-      data: string;
-      width: string;
-    }
-  ];
+  data: {
+    data: string | ReactElement;
+    width: string;
+  }[];
 }) {
   return (
     <div className="w-full h-8 border rounded-lg bg-muted">
       {data.map(({ data, width }, i) => (
-        <div key={i} className={width}>
+        <div key={i} className={cn(width, 'h-full align-middle')}>
           {data}
         </div>
       ))}
