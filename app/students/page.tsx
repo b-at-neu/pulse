e952d9b0a01aa195a2c9cd581ee71933e1588162
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 18:44:47 +0200

    #12 implement bar

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -1,7 +1,17 @@
-import { Title } from "@/components/ui/title";
+import { Bar } from '@/components/ui/bar';
+import { Title } from '@/components/ui/title';
+import { DoorClosed } from 'lucide-react';
 
 export default function Page() {
-  return <>
-    <Title>Students</Title>
-  </>
+  return (
+    <>
+      <Title>Students</Title>
+      <Bar
+        data={[
+          ['student name', 'w-1/2'],
+          [<DoorClosed key={0} />, 'w-1/2']
+        ]}
+      />
+    </>
+  );
 }
