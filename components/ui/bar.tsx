76daa955cb5f2b6301f9fd02ade8805ebf8b212b
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 21:41:07 +0200

    #13 change bar color on hover

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 
 export function Bar({ data, link }: { data: [data: string | ReactElement, width: string][]; link: Url }) {
   return (
-    <Link className="flex flex-row items-center gap-1 bg-muted w-full h-8 border rounded-lg px-3" href={''}>
+    <Link className="flex flex-row items-center gap-1 bg-muted hover:bg-muted/70 w-full h-8 border rounded-lg px-3" href={''}>
       {data.map(([data, width], i) => (
         <div key={i} className={cn(width, 'truncate')}>
           {data}
