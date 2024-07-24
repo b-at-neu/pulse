9d927ddf742fc65f3b040633261b52813721ab22
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 19:00:50 +0200

    #12 add truncating of text on overflow

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -1,13 +1,14 @@
+import { cn } from '@/lib/utils';
 import { ReactElement } from 'react';
 
 export function Bar({ data }: { data: [data: string | ReactElement, width: string][] }) {
   return (
-    <div className="flex flex-row items-center bg-muted w-full h-8 border rounded-lg px-3">
-      {data.map(([data, width], i) => 
-        <div key={i} className={width}>
+    <div className="flex flex-row items-center gap-1 bg-muted w-full h-8 border rounded-lg px-3">
+      {data.map(([data, width], i) => (
+        <div key={i} className={cn(width, 'truncate')}>
           {data}
         </div>
-      )}
+      ))}
     </div>
   );
 }
