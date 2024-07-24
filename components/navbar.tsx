891c523c62b91b6dca32f11501924e37f8267583
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 10:45:45 +0200

    #11: update structure

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,15 +9,15 @@ export function Navbar() {
   const [open, setOpen] = useState(false);
 
   return (
-    <nav className="h-12 bg-slate-900 flex justify-center">
+    <nav className="bg-slate-900 flex justify-center">
       <div className="md:w-3/5 h-full flex flex-col md:flex-row justify-around items-center">
         {open && <NavbarLinks />}
         <div className="flex items-center flex-row">
-          {!open && <p>home</p>}
+          {!open && <p className='p-3'>home</p>}
           <Button onMouseDown={() => setOpen(!open)}>
             {!open && <ChevronDown />}
             {open && <ChevronUp />}
-            <span className='sr-only'>Show Pages</span>
+            <span className="sr-only">Show Pages</span>
           </Button>
         </div>
       </div>
