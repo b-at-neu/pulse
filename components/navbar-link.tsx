5b0251aafc415bd45527408a276e198fe5294e23
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 10:45:52 +0200

    #11: add button

--- components/navbar-link.tsx
+++ components/navbar-link.tsx
@@ -1,9 +1,14 @@
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { cn } from '@/lib/utils';
+import { Button } from '@/components/ui/button';
 
 export function NavbarLink({ text, href }: { text: string; href: string }) {
+  const path = usePathname();
+
   return (
-    <Link className="h-fit hover:text-slate-400" href={href}>
-      {text}
-    </Link>
+    <Button className={cn('rounded')}>
+      <Link href={href}>{text}</Link>
+    </Button>
   );
 }
