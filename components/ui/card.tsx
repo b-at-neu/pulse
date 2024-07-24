93ed02135186a8ce6e2981a561734539329562d5
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 22:22:13 +0200

    #13 create card component

---
+++ components/ui/card.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from 'react';
+
+interface CardProps extends HTMLAttributes<HTMLElement> {
+  title: string;
+}
+
+export function Card({ title, children }: CardProps) {
+  return (
+    <div className="w-full bg-muted rounded-lg p-3">
+      <h2 className="font-bold text-center">{title}</h2>
+    </div>
+  );
+}
