c90d8c884b9ab5d7c9fa87c262abb5217be74710
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 22:22:27 +0200

    #13 add children

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -8,6 +8,7 @@ export function Card({ title, children }: CardProps) {
   return (
     <div className="w-full bg-muted rounded-lg p-3">
       <h2 className="font-bold text-center">{title}</h2>
+      {children}
     </div>
   );
 }
