745a49a8d08bc1b0ec043ffbfd5d2c76f49e861d
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 12:48:32 +0200

    #11: add logos

--- components/navbar-links.tsx
+++ components/navbar-links.tsx
@@ -1,13 +1,14 @@
 import { NavbarLink } from "@/components/navbar-link";
+import { House, User, Lock, BookKey, DoorOpen } from "lucide-react";
 
 export function NavbarLinks() {
   return (
     <>
-      <NavbarLink text={'home'} href={'/'} />
-      <NavbarLink text={'students'} href={'/students'} />
-      <NavbarLink text={'lockers'} href={'/lockers'} />
-      <NavbarLink text={'locks'} href={'/locks'} />
-      <NavbarLink text={'membership'} href={'/membership'} />
+      <NavbarLink text={'Home'} href={'/'} icon={<House />} />
+      <NavbarLink text={'Students'} href={'/students'} icon={<User />} />
+      <NavbarLink text={'Lockers'} href={'/lockers'} icon={<DoorOpen />} />
+      <NavbarLink text={'Locks'} href={'/locks'} icon={<Lock />} />
+      <NavbarLink text={'Membership'} href={'/membership'} icon={<BookKey />} />
     </>
   );
 }
