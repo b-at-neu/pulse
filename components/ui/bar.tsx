7b13691fbd1b2b86072f43381347c7bd6b9b482b
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 18:43:26 +0200

    #12 improve styling

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -1,14 +1,13 @@
 import { ReactElement } from 'react';
-import { cn } from '@/lib/utils';
 
 export function Bar({ data }: { data: [data: string | ReactElement, width: string][] }) {
   return (
-    <div className="w-full h-8 border rounded-lg bg-muted">
-      {data.map(([data, width], i) => (
-        <div key={i} className={cn(width, 'h-full align-middle')}>
+    <div className="flex flex-row items-center bg-muted w-full h-8 border rounded-lg px-3">
+      {data.map(([data, width], i) => 
+        <div key={i} className={width}>
           {data}
         </div>
-      ))}
+      )}
     </div>
   );
 }
