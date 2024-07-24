817c79f96727fdc33f01e4911860211f81c17354
b-at-neu <winkler.b@northeastern.edu>
2024-07-24 19:01:02 +0200

    #12 improve different screen formatting

--- app/students/page.tsx
+++ app/students/page.tsx
@@ -195,7 +195,7 @@ export default function Page() {
   return (
     <>
       <Title>Students</Title>
-      <div className="grid md:grid-cols-3 gap-3 mt-5">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-3 mt-5">
         {data.map((v, i) => (
           <Bar
             data={[
