668c8f304c1d9c28479f2e5d1b737f5f77770ff4
b-at-neu <winkler.b@northeastern.edu>
2024-08-18 15:01:46 +0200

    #13 write format

--- components/ui/bar.tsx
+++ components/ui/bar.tsx
@@ -4,7 +4,10 @@ import { cn } from '@/lib/utils';
 
 export function Bar({ data, link }: { data: [data: string | ReactElement, width: string][]; link: string }) {
   return (
-    <Link className="flex flex-row items-center gap-1 bg-muted hover:bg-muted/70 w-full h-8 border rounded-lg px-3" href={link}>
+    <Link
+      className="flex flex-row items-center gap-1 bg-muted hover:bg-muted/70 w-full h-8 border rounded-lg px-3"
+      href={link}
+    >
       {data.map(([data, width], i) => (
         <div key={i} className={cn(width, 'truncate')}>
           {data}
